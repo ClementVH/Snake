350a8cd4ef0db39fe8321aca33c78f4a6bca3550
ClementVH <clement.vanhecke@gmail.com>
2016-04-23 02:59:42 +0200

    Game is done now

---
+++ Snake.js
@@ -0,0 +1,92 @@
+function Snake(){
+    this.scale = 20;
+    this.pos = createVector(5,5);
+    
+    this.tail = [];
+    this.tail.push(createVector(4,5));
+    
+    this.dir = createVector(1,0);
+    
+    this.food = createVector(10,5)
+    
+    this.update = function(){
+        
+        for(var i = this.tail.length -1; i >= 0; i--){
+            
+            if(i == 0) this.tail[i] = this.pos.copy();
+            
+            else this.tail[i] = this.tail[i-1].copy();
+            
+        }
+        
+        this.pos.add(this.dir);
+        
+        if(this.pos.x == this.food.x && this.pos.y == this.food.y){
+            
+            this.addTail();
+            this.addFood();
+                
+        }
+        
+        if(this.pos.x * this.scale >= width) this.pos.x = 0;
+        if(this.pos.x * this.scale < 0) this.pos.x = width/this.scale;
+        if(this.pos.y * this.scale >= height) this.pos.y = 0;
+        if(this.pos.y * this.scale < 0) this.pos.y = height/this.scale;
+        
+    }
+    
+    this.display = function(){
+        
+        fill(255,0,0);
+        rect(this.pos.x * this.scale, this.pos.y * this.scale, this.scale, this.scale);
+        
+        fill(0,255,0);
+        
+        for(var i = 0; i < this.tail.length; i++){
+            
+            rect(this.tail[i].x * this.scale, this.tail[i].y * this.scale, this.scale, this.scale);
+            
+        }
+        
+        fill(0,0,255);
+        
+        rect(this.food.x * this.scale, this.food.y * this.scale, this.scale, this.scale);
+        
+        noFill();
+        
+    }
+    
+    this.addTail = function(){
+        
+        this.tail.push(createVector(-1,-1));
+        
+    }
+    
+    this.addFood = function(){
+        
+        ok = false;
+        
+        var newFood;
+        
+        while(!ok){
+            
+            var newFood = createVector(Math.floor(random(width/this.scale)), Math.floor(random(height/this.scale)));
+            
+            ok = true;
+            
+            for(var i = 0; i < this.tail.length; i++){
+            
+                if(this.tail[i].x == newFood.x && this.tail[i].y == newFood.y) ok = false;
+            
+            }
+            
+            if(this.pos.x == newFood.x && this.pos.y == newFood.y) ok = false;
+ 
+            
+        }
+        
+        this.food = newFood.copy();
+    
+    }
+    
+}
---
+++ sketch.js
@@ -0,0 +1,75 @@
+
+var snake;
+var compt = 0;
+var scl = 20;
+
+
+function setup() {
+  
+    createCanvas(1160,600);
+  
+    snake = new Snake();
+    
+    frameRate(5);
+  
+  
+  
+}
+
+function draw() {
+  
+    background(76);
+    
+    for(var x = 0; x <= width + scl + 50; x+= scl){
+        
+        line(x, 0, x, height);
+            
+    }
+  
+    for(var y = 0; y <= height + scl +50; y+= scl){
+        
+        line(0, y, width, y);
+      
+    }
+  
+    snake.update();
+    snake.display();
+    
+    if(compt > 0) compt --;
+    
+}
+
+function mousePressed(){
+    
+    snake.addTail();
+    
+}
+
+
+function keyPressed(){
+    
+    if((keyCode == DOWN_ARROW && compt == 0)){
+        var oppositeDir = createVector(0,-1);
+        if(oppositeDir.x != snake.dir.x && oppositeDir.y != snake.dir.y) snake.dir = createVector(0,1); 
+        compt = 1;
+    }
+    
+    else if((keyCode == UP_ARROW && compt == 0)){
+        var oppositeDir = createVector(0,1);
+        if(oppositeDir.x != snake.dir.x && oppositeDir.y != snake.dir.y) snake.dir = createVector(0,-1); 
+        compt = 1;
+    }
+    
+    else if((keyCode == LEFT_ARROW && compt == 0)){
+        var oppositeDir = createVector(1,0);
+        if(oppositeDir.x != snake.dir.x && oppositeDir.y != snake.dir.y) snake.dir = createVector(-1,0); 
+        compt = 1;
+    }
+    
+    else if((keyCode == RIGHT_ARROW && compt == 0)){
+        var oppositeDir = createVector(-1,0);
+        if(oppositeDir.x != snake.dir.x && oppositeDir.y != snake.dir.y) snake.dir = createVector(1,0); 
+        compt = 1;
+    }
+    
+}
